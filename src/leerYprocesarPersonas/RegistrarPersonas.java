43b70b1dd8e97ce1a01e9529e59c1373af7d6fe2
wzseba <wzseba@gmail.com>
2023-10-28 17:06:33 -0300

    add: en desarrollo, metodo getPersona

--- src/leerYprocesarPersonas/RegistrarPersonas.java
+++ src/leerYprocesarPersonas/RegistrarPersonas.java
@@ -1,5 +1,28 @@
 package leerYprocesarPersonas;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 public class RegistrarPersonas {
 
+	public List<Persona> getPersonas(String archivo) {
+		
+		File file = null;
+		BufferedReader fileBuffer = null;
+		FileReader fileReader = null;
+		LinkedList<Persona> listaPersonas = new LinkedList<Persona>();
+		
+		String line;
+		String [] datos;
+		
+		try {
+			
+		} catch (IOException e) {
+			
+		}
+	}
 }
