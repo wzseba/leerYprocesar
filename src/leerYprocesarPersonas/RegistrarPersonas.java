a19a53ec9d795dda2f17976d9e85b07dfb306d14
wzseba <wzseba@gmail.com>
2023-10-28 23:30:56 -0300

    add: metodo getPersonas finalizado, empezando metodo personas mayores

--- src/leerYprocesarPersonas/RegistrarPersonas.java
+++ src/leerYprocesarPersonas/RegistrarPersonas.java
@@ -9,20 +9,63 @@ import java.util.List;
 
 public class RegistrarPersonas {
 
-	public List<Persona> getPersonas(String archivo) {
+	public LinkedList<Persona> getPersonas(String archivo) {
+
+		File f = null;
+		FileReader fr = null;
+		BufferedReader br = null;
+		LinkedList<Persona> listaPersonas = new LinkedList<Persona>();
+
+		String linea;
+		String[] datos;
+
+		f = new File(archivo);
+		try {
+
+			fr = new FileReader(f);
+			br = new BufferedReader(fr);
+			linea = br.readLine();
+
+			while (linea != null) {
+				datos = linea.split(" ");
+
+//				int edad = Integer.parseInt(datos[2]);
+//				int dni = Integer.parseInt(datos[0]);
+//
+//				Persona p = new Persona(dni, datos[1], edad);
+//				listaPersonas.add(p);
+				
+				listaPersonas.add(new Persona(Integer.parseInt(datos[0]), datos[1], Integer.parseInt(datos[2])));
+				linea = br.readLine();
+			}
+			
+			fr.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return listaPersonas;
+	}
+	
+	public LinkedList<Persona> getPersonasMayoresAEdad(LinkedList<Persona> p , int edad){
 		
-		File file = null;
-		BufferedReader fileBuffer = null;
-		FileReader fileReader = null;
 		LinkedList<Persona> listaPersonas = new LinkedList<Persona>();
 		
-		String line;
-		String [] datos;
 		
-		try {
-			
-		} catch (IOException e) {
-			
+		
+		
+		return listaPersonas;
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		RegistrarPersonas rnp = new RegistrarPersonas();
+		
+		LinkedList<Persona> listaPersonas = rnp.getPersonas("personas.in");
+		
+		for (Persona persona : listaPersonas) {
+			System.out.println(persona);
 		}
 	}
 }
