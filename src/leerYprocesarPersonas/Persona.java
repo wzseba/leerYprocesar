e6be71f4b63c1110b02992895bc5baf1b46aac98
wzseba <wzseba@gmail.com>
2023-10-28 23:29:10 -0300

    add: se agrego constructor y getters

--- src/leerYprocesarPersonas/Persona.java
+++ src/leerYprocesarPersonas/Persona.java
@@ -1,5 +1,34 @@
 package leerYprocesarPersonas;
 
 public class Persona {
+	
+	private int dni;
+	private int edad;
+	private String nombre;
+	
+	public Persona(int dni, String nombre, int edad) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+	
+	public int getDni() {
+		return dni;
+	}
+	
+	public int getEdad() {
+		return edad;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	@Override
+	public String toString() {
+		return dni + " | " + edad + " | " + nombre;
+	}
+	
+	
 
 }
