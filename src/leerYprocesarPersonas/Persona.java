02a83abfe43d7e1a1581c02806d541cfbb35b9c7
wzseba <wzseba@gmail.com>
2023-10-29 18:29:45 -0300

    add: se agrego interface comparable

--- src/leerYprocesarPersonas/Persona.java
+++ src/leerYprocesarPersonas/Persona.java
@@ -1,25 +1,25 @@
 package leerYprocesarPersonas;
 
-public class Persona {
-	
+public class Persona implements Comparable<Persona> {
+
 	private int dni;
 	private int edad;
 	private String nombre;
-	
+
 	public Persona(int dni, String nombre, int edad) {
 		this.dni = dni;
 		this.nombre = nombre;
 		this.edad = edad;
 	}
-	
+
 	public int getDni() {
 		return dni;
 	}
-	
+
 	public int getEdad() {
 		return edad;
 	}
-	
+
 	public String getNombre() {
 		return nombre;
 	}
@@ -28,7 +28,12 @@ public class Persona {
 	public String toString() {
 		return dni + " | " + edad + " | " + nombre;
 	}
-	
-	
+
+	@Override
+	public int compareTo(Persona o) {
+
+		return this.nombre.compareTo(o.getNombre());
+
+	}
 
 }
